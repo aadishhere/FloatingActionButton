fdbe9324e1b3c692d7d084742dd2010e35a14935
Aadish Jain <aadishhere@gmail.com>
2025-05-14 14:12:42 +0530

    Initial Commit

---
+++ FloatingActionButton/FloatingActionButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingActionButtonApp.swift
+//  FloatingActionButton
+//
+//  Created by Aadish Jain on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FloatingActionButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
